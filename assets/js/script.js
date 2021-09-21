118b68e3f87ecc48875717ab560a8910cb21180f
Cygnet717 <kathybradt717@gmail.com>
2021-09-21 15:04:17 -0500

    added 10 questions

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+console.log('Loaded')
+
+
+
+/*GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score 
+separate page for high scores
+
+*/
+
+const questAndAns = [
+  {
+    question: 'Who is the first to die in the fight against Vegeta and Napa?',
+    answer: ['Tien', 'Yamcha', 'Krillin', 'Piccolo']
+  },
+  {
+    question: 'Who lives at Kame house?',
+    answer: ['Master Roshi', 'Gohan', 'Kami', 'Bulma']
+  },
+]
+
+/*
+questions
+
+Who cut off Vegetas tail?
+Krillen, Yagerobi, Gohan, Goku
+
+Which dragon ball was called 'Grandpa'?
+1 star, 7 star, 5 star, 4 star
+
+Is Vegeta the best character in all of Dragon Ball?
+Yes, 100%, Deffinitly, Of Course
+
+Who was the first saijn to go to ssj2?
+Goku, Vegeta, Gohan, Napa
+
+Which Symbol doesnt ever appear on Gokus gi?
+
+
+What is Goku's wifes name?
+Cha Cha, Cho cha, Chi Chi, Chi Cha
+
+
+Who is the best Character in DBZ?
+The Prince of all Saijns, Vegeta, Napas buddy, Bulmas man
+
+-Magic question
+Which is not a Magic The Gathering Keyworded ability?
+offering, epic, dash, horsemanship
+
+
+*/
