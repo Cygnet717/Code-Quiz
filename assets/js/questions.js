877d3853a20bf22df59b2c8581b236d2dfa6e4fe
Cygnet717 <kathybradt717@gmail.com>
2021-09-21 22:55:54 -0500

    all questions rendering in order and checking if answer entered is correct

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,43 +2,66 @@
 const questAndAns = [
   {
     question: 'Who is the first to die in the fight against Vegeta and Napa?',
-    answer: ['Tien', 'Yamcha', 'Krillin', 'Piccolo', 'Chiaotzu']
+    answer: ['Tien', 'Yamcha', 'Krillin', 'Piccolo']
   },
   {
     question: 'Who lives at Kame house?',
-    answer: ['Master Roshi', 'Gohan', 'Kami', 'Bulma', 'Suprime Kai']
+    answer: ['Master Roshi', 'Kami', 'Bulma', 'Suprime Kai']
   },
+  {
+    question: 'Who cut off Vegetas tail?',
+    answer: ['Krillen', 'Yagerobi', 'Gohan', 'Goku']
+  },
+  {
+    question: 'Which dragon ball was called "Grandpa"?',
+    answer: ['1 star', '7 star', '5 star', '4 star']
+  },
+  {
+    question: 'Is Vegeta the best character in all of Dragon Ball?',
+    answer: ['Yes', '100%', 'Deffinitly', 'Of Course']//Q5
+  },
+  {
+    question: 'Who was the first saiyan to go to ssj2?',
+    answer: ['Goku', 'Vegeta', 'Gohan', 'Napa']
+  },
+  {
+    question: 'Which Symbol doesnt ever appear on Gokus gi?',
+    answer: ['亀 Turtle Hermit', '神 Kami', '悟 Son Goku', '界王 King Kai']
+  },
+  {
+    question: "What is Goku's wifes name?",
+    answer: ['Cha Cha', 'Cho cha', 'Chi Chi', 'Chi Cha']
+  },
+  {
+    question: 'Who is the best Character in DBZ?',
+    answer: ['The Prince of all Saiyans', 'Vegeta', 'Napas buddy', 'Bulmas man']//Q9
+  },
+  {
+    question: 'Which is not a Magic The Gathering Keyworded ability?',
+    answer: ['offering', 'epic', 'dash', 'horsemanship'] //Q10
+  },
+]
+
+const correctAns = [
+  'Yamcha',
+  'Master Roshi',
+  'Yagerobi',
+  '4 star',
+  'all', 
+  'Gohan',
+  '神 Kami',
+  'Chi Chi',
+  'all',
+  'all'
 ]
 
 
 /*
-questions
-
-Who cut off Vegetas tail?
-Krillen, Yagerobi, Gohan, Goku
-
-Which dragon ball was called 'Grandpa'?
-1 star, 7 star, 5 star, 4 star
-
-Is Vegeta the best character in all of Dragon Ball?
-Yes, 100%, Deffinitly, Of Course
-
-Who was the first saijn to go to ssj2?
-Goku, Vegeta, Gohan, Napa
-
-Which Symbol doesnt ever appear on Gokus gi?
-
-
-What is Goku's wifes name?
-Cha Cha, Cho cha, Chi Chi, Chi Cha
-
-
-Who is the best Character in DBZ?
-The Prince of all Saijns, Vegeta, Napas buddy, Bulmas man
+qu
 
 -Magic question
-Which is not a Magic The Gathering Keyworded ability?
-offering, epic, dash, horsemanship
+
+
 
 
 */
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
-function log(say){
-  console.log(say)
-}
-
 let startBtn = document.querySelector("#start");
-let timeLeft = 4;
+let timeLeft = 20;
 let questionNumber = 0;
+let score = 0;
 let timerEl = document.querySelector('#timer');
 let questionEl = document.querySelector('#question');
 let answersEl = document.querySelector('#answersBox');
+let oneAnsEl = answersEl.children;
 let contentEl = document.querySelector('#contentBox');
+
+for(let i=0; i < oneAnsEl.length; i++){
+  oneAnsEl[i].addEventListener('click', function (eventTarget){
+    checkAnswer(eventTarget.path[0].innerHTML)
+  })
+}
+
+function checkAnswer(ans){
+ console.log(questionNumber)
+  if(questionNumber === 4 || questionNumber === 8){
+    console.log('all are right');
+    score++;
+    questionNumber++;
+  } else if(questionNumber === 9){
+    console.log('all are wrong :P');
+  } else if(ans === correctAns[questionNumber]){
+    score++;
+    questionNumber++;
+  } else {
+    console.log('wrong lost time')
+    timeLeft-5;
+    questionNumber++;
+  }
+
+  askQuestion();
+}
 
 
 function setTime(){
@@ -16,11 +40,15 @@ function setTime(){
     timeLeft--;
     timerEl.innerHTML = timeLeft;
 
-    if(timeLeft === 0){
+    if(timeLeft <= 0){
       clearInterval(timerInterval);
       //clear contentBox and populate with result?
     }
   }, 1000)
+}
+
+function chosenAnswer(e){
+  console.log(e)
 }
 
 function askQuestion(){
@@ -28,12 +56,11 @@ function askQuestion(){
   questionEl.innerHTML = questAndAns[questionNumber].question;
   let numbAns = questAndAns[questionNumber].answer;
   for(let i=0; i<numbAns.length; i++){
-    let newEl = document.createElement("p");
-    newEl.innerHTML = numbAns[i];
-    answersEl.appendChild(newEl);
+    answersEl.children[i].innerHTML= numbAns[i];
+  
   }
-//they answer
-//tally if right
+
+
 //increment question number
 //if last question end and show end view
 //if not run pose question again 
@@ -47,7 +74,10 @@ function beginQuiz(){
   //when timer ends
 }
 
-startBtn.addEventListener('click', beginQuiz)
+startBtn.addEventListener('click', beginQuiz);
+
+
+
 
 /*GIVEN I am taking a code quiz
 WHEN I click the start button
